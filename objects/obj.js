df86a86560e29e783f87caa7fea47790e4569867
Vivek <krishnacreations745@gmail.com>
2026-08-04 19:54:00 +0530

    little changes

--- objects/obj.js
+++ objects/obj.js
@@ -1,6 +1,14 @@
 const product = {
   name: "sock",
   price: 1000,
+  ["hello-world"]: 100,
+  rating: {
+    stars: 4.5,
+    count: 50,
+  },
+  fun: function function1() {
+    console.log("function inside an object");
+  },
 };
 
 console.log(product);
@@ -13,4 +21,15 @@ product.new = true;
 console.log(product);
 
 delete product.new;
+
 console.log(product);
+
+console.log(product["name"]);
+
+console.log(product["hello-world"]);
+
+console.log(product.rating.stars);
+
+product.fun();
+
+console.log(typeof console.log);
