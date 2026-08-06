d9a076367fe2b34ca9d7bd160377184270e273d7
Vivek <krishnacreations745@gmail.com>
2026-08-06 22:56:23 +0530

    learned funciton as a value

---
+++ advanced-functions-1/adv.js
@@ -0,0 +1,60 @@
+/*function greeting() {
+  console.log("hello");
+}
+
+greeting();
+
+const num = 2;
+
+const function1 = function () {
+  console.log("heelo2");
+}; //anonymus function
+
+console.log(function1);
+
+console.log(typeof function1);
+
+//we can save a function inside a variable
+
+function1();
+
+const o1 = {
+  num: 2,
+  fun: function () {
+    console.log("hello");
+  },
+};
+
+o1.fun();
+
+function display(param) {
+  console.log(param);
+}
+
+display(2);
+
+function run(param) {
+  param();
+}
+
+run(function () {
+  console.log("hello4");
+});
+
+
+
+//function are value we can do same as we do with value
+
+
+
+setTimeout(function () {
+  console.log("timeout");
+  console.log("timeout2");
+}, 3000); //1000milisecond=1sec
+
+console.log("next line");
+
+setInterval(function () {
+  console.log("interval");
+}, 3000);
+*/
