1f56e44906174bed7cf1ef3cf825011d458eeba0
Vivek <krishnacreations745@gmail.com>
2026-08-06 19:55:33 +0530

    added delete and date feature

--- to-do-1/to.js
+++ to-do-1/to.js
@@ -2,12 +2,14 @@ const todolist = [];
 
 function addtodo() {
   const inputElement = document.querySelector(".js-input");
+  const inputdate = document.querySelector(".js-date");
+  const datee = inputdate.value;
   const todo = inputElement.value;
-  if (todo !== "") {
-    todolist.push(todo);
+  if (todo && datee) {
+    todolist.push({ name: `${todo}`, duedate: `${datee}` });
     console.log(todolist);
-
     inputElement.value = "";
+    inputdate.value = "";
     rendertodolist();
   }
 }
@@ -21,9 +23,34 @@ function inputkeydown(event) {
 function rendertodolist() {
   let todolisthtml = "";
   for (let i = 0; i < todolist.length; i++) {
-    const todoname = todolist[i];
-    const html = `<p>${todoname}</p>`;
+    const todoname = todolist[i].name;
+    const toDueDate = todolist[i].duedate;
+
+    //or we can do
+    /*
+     const {name,duedate}=todolist[i]
+     then instead of using todoname and toduedate just direct use name and duedate
+     */
+
+    const html = `<p>
+    ${todoname}
+    ${toDueDate}
+
+
+
+
+
+    <button onclick='
+    
+    todolist.splice(${i},1);
+    rendertodolist();
+    
+    
+    '>delete</button>
+
+    </p>
+    `;
     todolisthtml += html;
   }
-  document.querySelector(".js-todo-list").innerHTML = todolisthtml;
+  document.querySelector(".js-tod-list").innerHTML = todolisthtml;
 }
