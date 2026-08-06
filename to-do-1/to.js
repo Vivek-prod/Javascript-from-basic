d6c8979bb5bd6e389c79b64286a9abc9184093c2
Vivek <krishnacreations745@gmail.com>
2026-08-06 15:49:18 +0530

    rendered to do list on the html page

--- to-do-1/to.js
+++ to-do-1/to.js
@@ -8,6 +8,7 @@ function addtodo() {
     console.log(todolist);
 
     inputElement.value = "";
+    rendertodolist();
   }
 }
 
@@ -15,4 +16,14 @@ function inputkeydown(event) {
   if (event.key === "Enter") {
     addtodo();
   }
+}
+
+function rendertodolist() {
+  let todolisthtml = "";
+  for (let i = 0; i < todolist.length; i++) {
+    const todoname = todolist[i];
+    const html = `<p>${todoname}</p>`;
+    todolisthtml += html;
+  }
+  document.querySelector(".js-todo-list").innerHTML = todolisthtml;
 }
